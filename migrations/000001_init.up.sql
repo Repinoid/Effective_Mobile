fefb8ff297261262b5d8d7d9bb187553511e41c3
“Naeel” <tazet@narod.ru>
2025-07-27 06:19:44 +0300

    1

--- migrations/000001_init.up.sql
+++ migrations/000001_init.up.sql
@@ -1,9 +1,18 @@
+-- CREATE TABLE IF NOT EXISTS subscriptions (
+--     id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+--     service_name VARCHAR(64) NOT NULL,
+--     price int NOT NULL,
+--     user_id VARCHAR(255) UNIQUE NOT NULL,
+--     start_date TIMESTAMP NOT NULL,
+--     end_date TIMESTAMP
+-- );
+
 CREATE TABLE IF NOT EXISTS subscriptions (
-    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    id INT GENERATED ALWAYS AS IDENTITY,
     service_name VARCHAR(64) NOT NULL,
     price int NOT NULL,
-    user_id VARCHAR(255) UNIQUE NOT NULL,
+    user_id VARCHAR(255) NOT NULL,
     start_date TIMESTAMP NOT NULL,
-    end_date TIMESTAMP
+    end_date TIMESTAMP,
+    PRIMARY KEY (user_id, service_name)
 );
-
